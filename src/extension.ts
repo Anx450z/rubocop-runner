c2b80a3fef94b9a4ef57dd2cef670ebe5b8e8c0b
Ankur <anx450z@gmail.com>
2023-05-01 02:47:42 +0530

    updated extention

--- src/extension.ts
+++ src/extension.ts
@@ -2,29 +2,39 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
+    const runTerminal = (rubocopCmd: string) => {
+        const terminals = vscode.window.terminals;
+        let rubocopTerminal = terminals.find(
+            terminal => terminal.name === 'Rubocop'
+        );
+        if (rubocopTerminal) {
+            rubocopTerminal.sendText(rubocopCmd, true);
+            rubocopTerminal.show();
+        } else {
+            rubocopTerminal = vscode.window.createTerminal({
+                name: 'Rubocop'
+            });
+            rubocopTerminal.sendText(rubocopCmd, true);
+            rubocopTerminal.show();
+        }
+    };
+
     context.subscriptions.push(
         vscode.commands.registerCommand('extension.runRubocop', () => {
             const editor = vscode.window.activeTextEditor;
             if (editor && editor.document.languageId === 'ruby') {
                 const filePath = editor.document.uri.fsPath;
                 const rubocopCmd = `rubocop ${filePath} -a`;
-                const terminal = vscode.window.createTerminal({
-                    name: 'Rubocop'
-                });
-                terminal.sendText(rubocopCmd, true);
-                terminal.show();
+                runTerminal(rubocopCmd);
             }
         }),
+
         vscode.commands.registerCommand('extension.runRubocopAll', () => {
             const editor = vscode.window.activeTextEditor;
             if (editor && editor.document.languageId === 'ruby') {
                 const filePath = editor.document.uri.fsPath;
                 const rubocopCmd = `rubocop ${filePath} -A`;
-                const terminal = vscode.window.createTerminal({
-                    name: 'Rubocop'
-                });
-                terminal.sendText(rubocopCmd, true);
-                terminal.show();
+                runTerminal(rubocopCmd);
             }
         })
     );
